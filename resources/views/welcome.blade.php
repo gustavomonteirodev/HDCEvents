1e277cd3f76b530ec6ffe8937683204a48ead9c4
Gustavo Monteiro de Assunção <monteirocontacto@gmail.com>
2022-12-09 08:59:41 -0300

    feature/home finalizada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,41 +1,32 @@
 @extends('layouts.main')
 
-@section('title','HDC Events' )
+@section('title', 'HDC Events')
 
 @section('content')
 
-<h1>  Frente </h1>
-<a href="/contact" class="relative flex items-top justify-center">ir para contato </a>
-@if (10 > 5)
-<p>A condição é true </p>
-@endif
-<p>Meu nome é {{$name}}</p>
-<p>Tenho {{$age}} anos</p>
-<p>Meu celular é um {{$smartphone}} de 2022</p>
+    <div id="search-container" class="col-md-12">
+        <h1>Busque um evento</h1>
+        <form action="">
+            <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+        </form>
+    </div>
 
-@if ($name == 'Pedro')
-<p>Meu nome é Pedro</p>
-@elseif($name == 'Gustavo')
-<p>Meu nome é <strong>{{$name}}</strong></p>
-@else
-<p>O nome não é Pedro</p>
-@endif
+    <div id="events-container" class="col-md-12">
+        <h2>Próximos Eventos</h2>
+        <p class="subtitle">Veja os eventos dos próximos dias</p>
+        <div id="cards-container" class="row">
+            @foreach ($events as $event)
+                <div class="card col-md-3">
+                    <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}">
+                    <div class="card-body">
+                    <p class="card-date"> 15/12/2022</p>
+                    <h5 class="card-title">{{ $event->title }}</h5>
+                    <p class="card-participants">X Participantes</p>
+                    <a href="#" class="btn btn-primary">Saber mais</a>
+                </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 
-@for($i = 0; $i < count($arr); $i++) <p>{{$arr[$i]}} - {{$i}}</p>
-
-    @endfor
-
-    @foreach ($names as $name )
-    <p>{{$loop->index}}</p>
-    <p>{{$name}}</p>
-
-    @endforeach
-
-    @php
-    $name = "Lucas";
-    echo $name;
-    @endphp
-
-    {{-- comentando aqui para testar --}}
-
-    @endsection
+@endsection
