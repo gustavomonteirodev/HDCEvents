8e7878581fcc99802beeb2b0297ae806dd28afe0
Gustavo Monteiro de Assunção <monteirocontacto@gmail.com>
2022-12-27 11:55:46 -0300

    feature/confirmar-participante-many-to-many

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -11,10 +11,19 @@
       </div>
       <div id="info-container" class="col-md-6">
         <h1>{{ $event->title }}</h1>
-        <p class="event-city"><ion-icon name="location-outline"></ion-icon> Localizado em {{ $event->city }}</p>
-        <p class="events-participants"><ion-icon name="people-outline"></ion-icon> X Participantes Confirmados</p>
-        <p class="event-owner"><ion-icon name="star-outline"></ion-icon> Criado por {{$eventOwner['name']}}</p>
-        <a href="#" class="btn btn-primary" id="event-submit">Confirmar Presença</a>
+        <p class="event-city"><ion-icon name="location-outline"></ion-icon> {{ $event->city }}</p>
+        <p class="events-participants"><ion-icon name="people-outline"></ion-icon> {{ count($event->users) }} Participantes</p>
+        <p class="event-owner"><ion-icon name="star-outline"></ion-icon> {{ $eventOwner['name'] }}</p>
+        <form action="/events/join/{{ $event->id }}" method="POST">
+          @csrf
+          <a href="/events/join/{{ $event->id }}"
+            class="btn btn-primary"
+            id="event-submit"
+            onclick="event.preventDefault();
+            this.closest('form').submit();">
+            Confirmar Presença
+          </a>
+        </form>
         <h3>O evento conta com:</h3>
         <ul id="items-list">
         @foreach($event->items as $item)
